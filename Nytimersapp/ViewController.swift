e39ad9371db3ea49500ac7fd5e9afa25533f4ef8
hasan al halabi <hasan.said.halabi@gmail.com>
2021-10-26 19:02:00 +0300

    Initial Commit

---
+++ Nytimersapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nytimersapp
+//
+//  Created by hasan al halabi on 26/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NytimersappUITests/NytimersappUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  NytimersappUITestsLaunchTests.swift
+//  NytimersappUITests
+//
+//  Created by hasan al halabi on 26/10/2021.
+//
+
+import XCTest
+
+class NytimersappUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
